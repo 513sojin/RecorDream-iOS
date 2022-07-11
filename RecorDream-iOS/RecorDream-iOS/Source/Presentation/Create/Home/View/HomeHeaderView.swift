28c3b7dd18b6aa6fcf7a1adc248dcb482e436b4a
SojinLee <513sojin@naver.com>
2022-07-11 21:52:03 +0900

    [Style] header view 추가 (#7)

---
+++ RecorDream-iOS/RecorDream-iOS/Source/Presentation/Create/Home/View/HomeHeaderView.swift
@@ -0,0 +1,57 @@
+//
+//  HomeHeaderView.swift
+//  RecorDream-iOS
+//
+//  Created by Sojin Lee on 2022/07/11.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+class HomeHeaderView: BaseView {
+    let logo = UIImageView().then {
+        $0.image = Constant.Image.icnRDSmallLogo
+    }
+    
+    let searchButton = UIButton().then {
+        $0.setImage(Constant.Image.icnSearch, for: .normal)
+    }
+    
+    let profileButton = UIButton().then {
+        $0.setImage(Constant.Image.icnProfile, for: .normal)
+    }
+    
+    lazy var iconStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 4
+        $0.addArrangedSubviews(searchButton, profileButton)
+    }
+    
+    override func setupView() {
+        addSubviews(logo,iconStackView)
+    }
+    
+    override func setupConstraint() {
+        logo.snp.makeConstraints { make in
+            make.width.equalTo(140)
+            make.height.equalTo(24)
+            make.leading.equalToSuperview().offset(11)
+            make.centerY.equalToSuperview()
+        }
+        
+        [searchButton,profileButton].forEach {
+            $0.snp.makeConstraints { make in
+                make.height.width.equalTo(24)
+            }
+        }
+        
+        iconStackView.snp.makeConstraints { make in
+            make.width.equalTo(52)
+            make.height.equalTo(24)
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(16)
+        }
+    
+    }
+}
--- RecorDream-iOS/RecorDream-iOS/Source/Presentation/Create/Home/ViewControllers/HomeViewController.swift
+++ RecorDream-iOS/RecorDream-iOS/Source/Presentation/Create/Home/ViewControllers/HomeViewController.swift
@@ -6,24 +6,28 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class HomeViewController: BaseViewController {
 
+    let headerView = HomeHeaderView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func setHierarchy() {
+        view.addSubview(headerView)
     }
-    */
+    
+    private func setupConstraints() {
+        headerView.snp.makeConstraints { make in
+            make.height.equalTo(68)
+            make.leading.trailing.top.equalTo(view.safeAreaLayoutGuide)
+        }
+
+    }
 
 }
